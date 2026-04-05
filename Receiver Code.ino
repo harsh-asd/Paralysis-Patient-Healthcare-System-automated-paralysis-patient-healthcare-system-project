db5dbdbcabc5e92eb2d51835e266767d4caf8dd9
harsh-asd <singh.harsh.asd@gmail.com>
2026-04-05 12:34:57 +0530

    Add files via upload

---
+++ Receiver Code.ino
@@ -0,0 +1,160 @@
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+#include <RCSwitch.h>
+
+SoftwareSerial mySerial(8, 9);
+
+LiquidCrystal lcd(A5, 3, 4, 5, 6, 7);
+
+RCSwitch mySwitch = RCSwitch();
+
+int flag;
+
+char C;
+
+int value;
+int Buzzer = A0;
+
+long Time;
+
+void setup(){
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  mySwitch.enableReceive(0); 
+  delay(100);
+
+  lcd.begin(16, 2);
+  lcd.print("  HEALTH  CARE  ");
+
+  flag = 0;
+  delay(5000);
+
+  pinMode(Buzzer,OUTPUT);
+}
+
+
+void loop(){
+  if (mySwitch.available()) {   
+    value = mySwitch.getReceivedValue();
+    mySwitch.resetAvailable();
+    digitalWrite(13,HIGH);
+    Serial.println(value);
+    delay(10);
+    digitalWrite(13,LOW);
+  }
+  
+  lcd.setCursor(0,0);
+  
+  if     ( value == 1111){digitalWrite(Buzzer,HIGH); delay(10);lcd.print("CALL ATTENDENT ");digitalWrite(Buzzer,LOW);delay(10);if (millis()-Time > 10000){SendMessageA();Time = millis();}}
+  else if( value == 2222){digitalWrite(Buzzer,HIGH); delay(10);lcd.print("I NEED WATER   ");digitalWrite(Buzzer,LOW);delay(10);if (millis()-Time > 10000){SendMessageB();Time = millis();}}
+  else if( value == 3333){digitalWrite(Buzzer,HIGH); delay(10);lcd.print("I AM HUNGRY    ");digitalWrite(Buzzer,LOW);delay(10);if (millis()-Time > 10000){SendMessageC();Time = millis();}}
+  else if( value == 4444){digitalWrite(Buzzer,HIGH); delay(10);lcd.print("SOME PROBLEM   ");digitalWrite(Buzzer,LOW);delay(10);if (millis()-Time > 10000){SendMessageD();Time = millis();}}
+  else if( value == 9999){lcd.print(" NO COMMAND     ");Time = millis(); }
+}
+
+
+void SendMessageA(){
+  lcd.setCursor(0,1);
+  lcd.print("MAIL SENDING  ");
+  mySerial.println("AT+CMGF=1");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(1000);
+   
+  mySerial.println("AT+CMGS=\"+919001008233\"\r"); 
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  
+  mySerial.println(" CALL ATTENDENT ");
+  
+  digitalWrite(Buzzer,HIGH);  
+  delay(100);
+  
+  mySerial.println((char)26);
+  lcd.setCursor(0,1);
+  lcd.print("SENT            ");
+
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void SendMessageB(){
+  lcd.setCursor(0,1);
+  lcd.print("MAIL SENDING   ");
+  mySerial.println("AT+CMGF=1");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(1000);
+  
+  mySerial.println("AT+CMGS=\"+919001008233\"\r"); 
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  
+  mySerial.println(" I NEED WATER ");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(100);
+  mySerial.println((char)26);
+  lcd.setCursor(0,1);
+  lcd.print("SENT            ");
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void SendMessageC(){
+  lcd.setCursor(0,1);
+  lcd.print("MAIL SENDING   ");
+  mySerial.println("AT+CMGF=1");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(1000);
+   
+  mySerial.println("AT+CMGS=\"+919001008233\"\r"); 
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  mySerial.println(" I AM HUNGRY ");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(100);
+  mySerial.println((char)26);
+  lcd.setCursor(0,1);
+  lcd.print("SENT            ");
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void SendMessageD(){
+  lcd.setCursor(0,1);
+  lcd.print("MAIL SENDING   ");
+  mySerial.println("AT+CMGF=1");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(1000);
+  mySerial.println("AT+CMGS=\"+919001008233\"\r"); 
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  mySerial.println(" SOME PROBLEM ");
+  
+  digitalWrite(Buzzer,HIGH);
+  delay(100);
+  mySerial.println((char)26);
+  lcd.setCursor(0,1);
+  lcd.print("SENT            ");
+  
+  digitalWrite(Buzzer,LOW);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
---
+++ Transmitter Code.ino
@@ -0,0 +1,70 @@
+#include <EEPROM.h>
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+int val1;
+int val2;
+
+int valX;
+int valY;
+
+int SensorX = A5;
+int SensorY = A6;
+
+int Switch = 7;
+
+int LED1 = 8;
+int LED2 = 9;
+int LED3 = A0;
+int LED4 = A1;
+
+void setup() {
+  Serial.begin(9600);
+  
+  mySwitch.enableTransmit(4);
+
+   
+  pinMode(SensorX , INPUT);
+  pinMode(SensorY , INPUT);
+
+  pinMode(LED1 , OUTPUT);
+  pinMode(LED2 , OUTPUT);
+  pinMode(LED3 , OUTPUT);
+  pinMode(LED4 , OUTPUT);
+  
+  pinMode(Switch , INPUT_PULLUP);
+
+  if (digitalRead(Switch) == LOW){
+    val1 = analogRead(SensorX)/4;
+    val2 = analogRead(SensorY)/4;
+    
+    EEPROM.write(0, val1);
+    EEPROM.write(1, val2);
+    
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+  }
+}
+
+void loop() {
+  valX = analogRead(SensorX)/4;
+  valY = analogRead(SensorY)/4;
+
+  if      (EEPROM.read(0) + 10 < valX ) { digitalWrite(LED1,HIGH); mySwitch.send(1111, 24);}  
+  
+  else if (EEPROM.read(0) - 10 > valX ) { digitalWrite(LED2,HIGH); mySwitch.send(2222, 24);}
+  else if (EEPROM.read(1) + 10 < valY ) { digitalWrite(LED3,HIGH); mySwitch.send(3333, 24);}
+  else if (EEPROM.read(1) - 10 > valY ) { digitalWrite(LED4,HIGH); mySwitch.send(4444, 24);} 
+  
+  else                                  { digitalWrite(LED1,LOW); 
+                                          digitalWrite(LED2,LOW);
+                                          digitalWrite(LED3,LOW);
+                                          digitalWrite(LED4,LOW);  
+                                          
+                                         mySwitch.send(9999, 24); }
+  delay(300);
+}
